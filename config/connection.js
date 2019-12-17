36dfa6281ba94f5f1c6ce92270256ff2121c3987
sara sullivan <saralsullivan@gmail.com>
2019-12-17 18:21:01 -0500

    port password change

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "mysq!Fr33man!",
+  password: "password",
   database: "burgers_db"
 });
 
